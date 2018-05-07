8339f3aa8b00ac87b61c3d5f53cd53b563c1fec9
Edd <32267216+EPurslow2017@users.noreply.github.com>
2018-05-07 21:13:21 +0100

    Changes to code

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,7 @@
-<!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
-
-$(document).on("pagecreate","#pageone",function(){
-  
- 	});
+window.onload = function() {
+    
+   
+   function diceRoll() {
+      var dice = Math.Ceil(Math.random()*4);
+      document.getElementById("diceTest").
+};
