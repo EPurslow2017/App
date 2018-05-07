2fed46627e19c73042a80dcea60a515a8ee8df55
Edd <32267216+EPurslow2017@users.noreply.github.com>
2018-05-08 00:02:49 +0100

    Finishing touches added
    Final touches to game added

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,77 @@
-window.onload = function() {
+function rolledPlayer4P() {
+   var playerRoll = Math.ceil(Math.random()*4);
     
-   
-   function diceRoll() {
-      var dice = Math.Ceil(Math.random()*4);
-      document.getElementById("diceTest").
+   if (playerRoll == 1) {
+      document.getElementById('4pColour').src = "blue.png";
+   }
+   else if (playerRoll == 2) {
+      document.getElementById('4pColour').src = "yellow.png";
+   }
+   else if (playerRoll == 3) {
+      document.getElementById('4pColour').src = "red.png";
+   }
+   else if (playerRoll == 4) {
+      document.getElementById('4pColour').src = "green.png";
+   };
 };
+function rolledDrink4P() {
+   var drinkRoll = Math.ceil(Math.random()*15);
+   document.getElementById('4pDrink').innerHTML = drinkRoll; 
+};
+
+
+function rolledPlayer5P() {
+   var playerRoll = Math.ceil(Math.random()*5);
+    
+   if (playerRoll == 1) {
+      document.getElementById('5pColour').src = "blue.png";
+   }
+   else if (playerRoll == 2) {
+      document.getElementById('5pColour').src = "yellow.png";
+   }
+   else if (playerRoll == 3) {
+      document.getElementById('5pColour').src = "red.png";
+   }
+   else if (playerRoll == 4) {
+      document.getElementById('5pColour').src = "green.png";
+   }
+   else if (playerRoll == 5) {
+      document.getElementById('5pColour').src = "purple.png";
+   };
+};
+function rolledDrink5P() {
+   var drinkRoll = Math.ceil(Math.random()*15);
+   document.getElementById('5pDrink').innerHTML = drinkRoll; 
+};
+
+
+function rolledPlayer6P() {
+   var playerRoll = Math.ceil(Math.random()*6);
+    
+   if (playerRoll == 1) {
+      document.getElementById('6pColour').src = "blue.png";
+   }
+   else if (playerRoll == 2) {
+      document.getElementById('6pColour').src = "yellow.png";
+   }
+   else if (playerRoll == 3) {
+      document.getElementById('6pColour').src = "red.png";
+   }
+   else if (playerRoll == 4) {
+      document.getElementById('6pColour').src = "green.png";
+   }
+   else if (playerRoll == 5) {
+      document.getElementById('6pColour').src = "purple.png";
+   }
+   else if (playerRoll == 6) {
+      document.getElementById('6pColour').src = "pink.png";
+   };
+};
+function rolledDrink6P() {
+   var drinkRoll = Math.ceil(Math.random()*15);
+   document.getElementById('6pDrink').innerHTML = drinkRoll; 
+};
+
+
+
+
